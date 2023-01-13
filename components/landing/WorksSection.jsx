e95e3e9ceb24d4f1e637a4ef29468018243b4aff
Donmikko Dan M. Olmillo <57443799+mikkolmillo@users.noreply.github.com>
2023-01-13 13:13:19 +0800

    add navigation on works section

--- components/landing/WorksSection.jsx
+++ components/landing/WorksSection.jsx
@@ -1,5 +1,14 @@
 import Heading from '../ui/Heading'
 
+const tabs = [
+  { name: 'Show All', href: '#', current: false },
+  { name: 'Architecture', href: '#', current: false },
+  { name: 'Construction', href: '#', current: true },
+  { name: 'Consulting', href: '#', current: false },
+  { name: 'Electrical', href: '#', current: false },
+  { name: 'Metal Roofing', href: '#', current: false },
+  { name: 'Painting', href: '#', current: false },
+]
 const files = [
   {
     title: 'IMG_4985.HEIC',
@@ -40,12 +49,45 @@ const files = [
   // More files...
 ]
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 const WorksSection = () => {
   return (
     <div className='relative'>
       <Heading
         title={'Recent Works'}
       />
+      <div className="sm:hidden pb-16">
+        <label htmlFor="tabs" className="sr-only">
+          Select a tab
+        </label>
+        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+        <select
+          id="tabs"
+          name="tabs"
+          className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          defaultValue={tabs.find((tab) => tab.current).name}
+        >
+          {tabs.map((tab) => (
+            <option key={tab.name}>{tab.name}</option>
+          ))}
+        </select>
+      </div>
+      <div className="hidden sm:block pb-16">
+        <nav className="-mb-px flex space-x-8 justify-center" aria-label="Tabs">
+          {tabs.map((tab) => (
+            <button
+              key={tab.name}
+              className='border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
+              aria-current={tab.current ? 'page' : undefined}
+            >
+              {tab.name}
+            </button>
+          ))}
+        </nav>
+      </div>
       <div>
         <ul role="list" className="max-w-7xl mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
           {files.map((file) => (
