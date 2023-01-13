85397d623f81e3a2ea66c68d3e2c89d0289b7003
Donmikko Dan M. Olmillo <57443799+mikkolmillo@users.noreply.github.com>
2023-01-13 10:13:28 +0800

    design services

--- components/landing/ServicesSection.jsx
+++ components/landing/ServicesSection.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Heading from '../ui/Heading'
 
 import {
@@ -19,11 +20,63 @@ const navigation = [
   { name: 'Green Housing', icon: ChartBarIcon, href: '#', current: false },
 ]
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+const services = [
+  {
+    img: 'https://aadi.theironnetwork.org/wp-content/uploads/2016/06/44639686_xxl-1-940x600.jpg',
+    title: 'Construction',
+    content: 'History of construction is truly remarkable.  From neolithic architecture to modern day buildings,from mud houses to high rise buildings , the tools and methods have evolved drastically.Archaeological records and  surveys provide us with glimpse of structure that were built thousands'
+  },
+  {
+    img: 'https://aadi.theironnetwork.org/wp-content/uploads/2016/06/42904914_xxl-940x600.jpg',
+    title: 'Architecture',
+    content: 'What is architecture , is it just an outlook , an opinion , a perspective or is it more than that ?? Some of the  early examples of architecture might shed some light.It can be sufficely presumed  by looking at'
+  },
+  {
+    img: 'https://aadi.theironnetwork.org/wp-content/uploads/2016/06/42895685_xxl-1-940x600.jpg',
+    title: 'Consulting',
+    content: "Planning does not get better by chance it get's better  on the ability to adapt and implement those changes.Project planning is an aspect of construction that require years of experience in construction,keep a project running in budget, handle problems ,creating"
+  },
+  {
+    img: 'https://aadi.theironnetwork.org/wp-content/uploads/2016/06/Roofing-940x600.jpg',
+    title: 'Metal Roofing',
+    content: "Though not the first thing you notice,yet  a roof  has much to say about a home's style.Roof shapes and material differ widely , depending upon the usage, and from region to region, contingent upon the climatic conditions.Considering the traditional roof"
+  },
+  {
+    img: 'https://aadi.theironnetwork.org/wp-content/uploads/2016/06/Paint-1-940x600.jpg',
+    title: 'Painting',
+    content: "AADI construction started in 2014 as a painting contractor, ever since then we have been actively engaged at offering diverse range of services, building painting , renovation, industrial painting,home painting and waterproofing.We deal with texture painting,interior as well as exterior"
+  },
+  {
+    img: 'https://aadi.theironnetwork.org/wp-content/uploads/2016/06/37784013_xxl1-940x600.jpg',
+    title: 'Electrical',
+    content: "Still struggling with old-fashioned and defunct electrical system? Are you looking for customized electrical solution for your home and enterprise that operates hassle free and requires zero maintenance?? As an Electrical contractor we at AADI specialize in  design , remodelling"
+  },
+  {
+    img: 'https://aadi.theironnetwork.org/wp-content/uploads/2016/06/Green-house-940x600.jpg',
+    title: 'Green Housing',
+    content: "Green housing or green construction refers to a structure whose design, construction and operation is sustainable , resource efficient ,and the design and material used is such that it allows indoor environmental quality enhancement, operations and maintenance optimization, waste reduction,structure"
+  }
+]
 
 const ServicesSection = () => {
+  const [content, setContent] = useState({
+    img: 'https://aadi.theironnetwork.org/wp-content/uploads/2016/06/44639686_xxl-1-940x600.jpg',
+    title: 'Construction',
+    text: ''
+  })
+
+  const selectHandler = (selectItem) => {
+    const selectedContent = services.filter(item => item.title === selectItem)
+    const contentJson = {
+      img: selectedContent[0].img,
+      title: selectedContent[0].title,
+      content: selectedContent[0].content,
+    }
+
+    console.log(selectedContent);
+    setContent(contentJson)
+  }
+
   return (
     <div className="relative bg-white py-24">
       <div className="mx-auto max-w-md px-6 text-center sm:max-w-3xl lg:max-w-7xl lg:px-8">
@@ -35,25 +88,17 @@ const ServicesSection = () => {
           <div className="lg:grid lg:gap-12 lg:grid-cols-3">
             <div className="hidden lg:block grid-cols-1">
               {navigation.map((item) => (
-                <a
+                <button
                   key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-indigo-50 border-indigo-600 text-indigo-600'
-                      : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                    'group flex items-center px-3 py-7 text-sm font-medium border-l-4'
-                  )}
+                  className='border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-3 py-7 text-sm font-medium border-l-4 w-full'
+                  onClick={() => selectHandler(item.name)}
                 >
                   <item.icon
-                    className={classNames(
-                      item.current ? 'text-indigo-500' : 'text-gray-400 group-hover:text-gray-500',
-                      'mr-3 flex-shrink-0 h-6 w-6'
-                    )}
+                    className='text-gray-400 group-hover:text-gray-500 mr-3 flex-shrink-0 h-6 w-6'
                     aria-hidden="true"
                   />
                   {item.name}
-                </a>
+                </button>
               ))}
             </div>
             {/* <div className="flex items-center justify-between lg:hidden">
@@ -67,8 +112,14 @@ const ServicesSection = () => {
                       </a>
                     ))}
                   </div> */}
-            <div className="h-32 lg:h-full lg:grid-cols-2 lg:col-span-2 bg-black">
-              hello
+            <div className="h-32 lg:h-full lg:grid-cols-2 lg:col-span-2">
+              {content && (
+                <div className='flex flex-col items-start'>
+                  <img src={content.img} alt={content.title} className='object-cover aspect-2' />
+                  <h3 className='py-3 text-lg'>{content.title}</h3>
+                  <p className='text-left'>{content.content}</p>
+                </div>
+              )}
             </div>
           </div>
         </div>
